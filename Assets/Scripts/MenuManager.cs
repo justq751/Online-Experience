e296f6609ea93bf40a873ad4bea2cbb535e9af0e
VladD <dmitriev.vs.751@mail.ru>
2020-05-19 18:05:09 +0300

    Established connection and user name on cnnection

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -10,4 +10,45 @@ public class MenuManager : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject UserNameScreen, ConnectScreen;
     [SerializeField] private GameObject CreateUserNameButton;
     [SerializeField] private InputField UserNameInput, CreateRoomInput, JoinRoomInput;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() //first called after successful connection
+    {
+        Debug.Log("Connected to master!");
+        PhotonNetwork.JoinLobby(TypedLobby.Default); //check documentation
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Connected to lobby!");
+        UserNameScreen.SetActive(true);
+    }
+
+    #region UIMethods
+
+    public void OnClick_CreateNameButton()
+    {
+        if (UserNameInput.text.Length >= 2)
+        {
+            PhotonNetwork.NickName = UserNameInput.text;
+        }
+    }
+
+    public void OnNameFieldChanged()
+    {
+        if(UserNameInput.text.Length >= 2)
+        {
+            CreateUserNameButton.SetActive(true);
+        }
+        else
+        {
+            CreateUserNameButton.SetActive(false);
+        }
+    }
+
+    #endregion
 }
