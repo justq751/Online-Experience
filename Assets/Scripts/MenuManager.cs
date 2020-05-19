b7f5d338a6639941e7837c6a3d6ac80717b3ac23
VladD <dmitriev.vs.751@mail.ru>
2020-05-19 17:52:34 +0300

    Script dummy

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject UserNameScreen, ConnectScreen;
+    [SerializeField] private GameObject CreateUserNameButton;
+    [SerializeField] private InputField UserNameInput, CreateRoomInput, JoinRoomInput;
+}
