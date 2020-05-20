8128088be3b62f1728208ac15126c4f86ac9b34d
VladD <dmitriev.vs.751@mail.ru>
2020-05-20 19:44:00 +0300

    Connection working and position is synced

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Character : MonoBehaviourPun
+{
+    public GameObject playerCamera;
+    public float moveSpeed = 1.0f;
+    public SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        if (photonView.IsMine)
+        {
+            playerCamera.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            CheckInput();
+        }
+    }
+
+    private void CheckInput()
+    {
+        var movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            sprite.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            sprite.flipX = true;
+        }
+    }
+}
