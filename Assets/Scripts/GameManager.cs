d1b4807ad833864e940b3472b0b6a90d0622490c
VladD <dmitriev.vs.751@mail.ru>
2020-08-20 13:49:24 +0300

    Ping tracking and health bar sprite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject canvas;
     public GameObject sceneCamera;
+    public Text pingRate;
 
     private void Awake()
     {
         canvas.SetActive(true);
+    }
+
+    private void Update()
+    {
+        pingRate.text = "Network ping: " + PhotonNetwork.GetPing();
     }
 
     public void SpawnPlayer()
