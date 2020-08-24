6e94e2debea9f01e471828ba43865c98adaca9b3
VladD <dmitriev.vs.751@mail.ru>
2020-08-24 18:29:52 +0300

    Code for dying and started ui for death and respawning

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health : MonoBehaviourPun
 {
     public Image fillImage;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public BoxCollider2D boxCol;
+
+    public float health;
+
+    public void CheckHealth()
+    {
+        if (photonView.IsMine && health <= 0)
+        {
+            this.GetComponent<PhotonView>().RPC("Death", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    public void Death()
+    {
+        rb.gravityScale = 0;
+        boxCol.enabled = false;
+        sr.enabled = false;
+    }
+
+    [PunRPC]
+    public void Revive()
+    {
+        rb.gravityScale = 1;
+        boxCol.enabled = true;
+        sr.enabled = true;
+    }
 
     [PunRPC]
     public void HealthUpdate(float damage)
     {
         fillImage.fillAmount -= damage;
+        health = fillImage.fillAmount;
+        CheckHealth();
     }
 }
