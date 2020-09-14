8e079f10a3309222831babd35447071b07d1bcb2
VladD <dmitriev.vs.751@mail.ru>
2020-09-14 11:51:17 +0300

    Before camera changes

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothspeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
