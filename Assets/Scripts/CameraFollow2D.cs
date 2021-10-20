2ed49f0e4db7c22a6a2a44fe21172a5ff2bd438c
Vladislav <ironman751@yandex.ru>
2021-10-20 11:27:52 +0300

    minor

--- Assets/Scripts/CameraFollow2D.cs
+++ Assets/Scripts/CameraFollow2D.cs
@@ -7,9 +7,12 @@ public class CameraFollow2D : MonoBehaviour
     public Transform target;
 
     public float smoothspeed = 0.125f;
+    public Vector3 offset;
 
     private void LateUpdate()
     {
-        transform.position = target.position;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothspeed * Time.deltaTime);
+        transform.position = smoothedPosition;
     }
 }
